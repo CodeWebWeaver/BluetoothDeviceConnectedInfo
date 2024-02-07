8f73ff05be602de7471ef3189189b65df825df14
Alexander Parkhomovsky <69048440+CodeWebWeaver@users.noreply.github.com>
2024-02-07 18:21:51 +0200

    Implemented color picker and parameter gathering functionality with input validation.

--- app/src/main/java/com/example/eyeimprove/ControlPanelActivity.kt
+++ app/src/main/java/com/example/eyeimprove/ControlPanelActivity.kt
@@ -10,12 +10,21 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.example.eyeimprove.databinding.ControlScreenBinding
 import java.lang.reflect.Method
 import java.util.UUID
 
@@ -33,6 +42,41 @@ class ControlPanelActivity : AppCompatActivity() {
     private val UUID_STRING_WELL_KNOWN : String = "00001101-0000-1000-2000-00805F9B34FB"
     private val WELL_KNOWN_UUID: UUID = UUID.fromString(UUID_STRING_WELL_KNOWN)
     private lateinit var bluetoothSocket: BluetoothSocket
+
+    /** Inputs from connected Device  */
+    //Views Inputs
+    private lateinit var temp_output_filler : TextView
+    private lateinit var humidify_output_filler : TextView
+    private lateinit var frequency_output_filler : TextView
+    private lateinit var light_intensity_output_filler : TextView
+    private lateinit var color_input_card : CardView
+
+    // Params inputs
+    private var temperature_input : Int? = null // null or something
+    private var humidify_input : Int? = null // null or something
+    private var frequency_input : Int? = null // null or something
+    private var light_intensity_input : Int? = null // null or something
+    private var color_input : String? = null // null or something
+
+    /** Outputs to Device  */
+    // Views outputs
+    private lateinit var temp_input_field : EditText
+    private lateinit var humidify_input_field : EditText
+    private lateinit var frequency_input_field : EditText
+    private lateinit var light_intensity_input_field : EditText
+    private lateinit var colorSpinner : Spinner
+
+    // Params outputs
+    private var temperature_output : Int? = null // 15-25
+    private var humidify_output : Int? = null //0 - 1
+    private var frequency_output : Int? = null //10 - 900
+    private var light_intensity_output : Int? = null //0 - 1
+    private var color_output : String? = null //#252525
+
+    // Color Spinner
+    lateinit var binding: ControlScreenBinding
+    lateinit var selectedColor: ColorObject
+
 
     private val bluetoothPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -68,24 +112,28 @@ class ControlPanelActivity : AppCompatActivity() {
             addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
         }
         registerReceiver(bluetoothReceiver, filter)
-    }
 
+        initializeParams()
+        resetParams()
+    }
     override fun onStop() {
         super.onStop()
         unregisterReceiver(bluetoothReceiver)
+
+        resetParams()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.control_screen)
+        binding = ControlScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadColorSpinner()
 
         checkBluetoothPermission()
         bluetoothManager = getSystemService(BluetoothManager::class.java) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
 
-        connectedDeviceAddress = intent.getStringExtra("selectedDeviceAddress")
-        connectedDevice = getConnectedDevise(connectedDeviceAddress)
-
+        connectedDevice = getConnectedDevice()
         Log.i("INFO", "Було отримано пристрій ${connectedDevice?.name} \n з адресом ${connectedDevice?.address}")
 
         val returnButton = findViewById<Button>(R.id.control_screen_return_button)
@@ -102,15 +150,49 @@ class ControlPanelActivity : AppCompatActivity() {
 
         val submitButton = findViewById<Button>(R.id.control_screen_submit_button)
         submitButton.setOnClickListener {
-
+            gatherInputs()
         }
     }
 
-    private fun getConnectedDevise(address: String?): BluetoothDevice? {
+    private fun gatherInputs() {
+        temperature_output = validateAndGatherInput(temp_input_field, 15, 25) // 15-25
+        humidify_output = validateAndGatherInput(humidify_input_field, 0, 100) //0 - 1
+        frequency_output = validateAndGatherInput(frequency_input_field, 10, 900) // 10 - 900
+        light_intensity_output = validateAndGatherInput(light_intensity_input_field, 0, 100) //0 - 1
+
+    }
+
+    private fun validateAndGatherInput(editText: EditText?, minValue: Int, maxValue: Int): Int? {
+        val input = editText?.text?.toString()
+        if (input.isNullOrEmpty()) {
+            editText?.backgroundTintList = ColorStateList.valueOf(Color.YELLOW)
+            return null
+        }
+
+        val inputValue = input.toIntOrNull()
+        if (inputValue == null) {
+            // Если ввод пользователя не удалось преобразовать в Int, подчеркиваем поле другим цветом
+            editText?.backgroundTintList = ColorStateList.valueOf(Color.RED)
+            return null
+        }
+
+        if (inputValue !in minValue..maxValue) {
+            // Если ввод пользователя не входит в диапазон, подчеркиваем поле другим цветом
+            editText?.backgroundTintList = ColorStateList.valueOf(Color.RED)
+            return null
+        }
+
+        editText?.backgroundTintList = ColorStateList.valueOf(Color.BLACK)
+        return inputValue
+    }
+
+
+    private fun getConnectedDevice(): BluetoothDevice? {
+        connectedDeviceAddress = intent.getStringExtra("selectedDeviceAddress")
         checkBluetoothPermission()
         return bluetoothAdapter.bondedDevices
             .filter { isConnected(it) }
-            .firstOrNull { it.address == address }
+            .firstOrNull { it.address == connectedDeviceAddress }
     }
 
     private fun checkBluetoothPermission() {
@@ -139,5 +221,86 @@ class ControlPanelActivity : AppCompatActivity() {
         } catch (e: Exception) {
             throw IllegalStateException(e)
         }
+    }
+
+    private fun loadColorSpinner()
+    {
+        selectedColor = ColorList().defaultColor
+        binding.colorSpinner.apply {
+            adapter = ColorSpinnerAdapter(applicationContext, ColorList().basicColors())
+            setSelection(ColorList().colorPosition(selectedColor), false)
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener
+            {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long)
+                {
+                    selectedColor = ColorList().basicColors()[position]
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {}
+            }
+        }
+    }
+
+    private fun initializeParams() {
+        // Инициализация полей ввода
+        temp_input_field = findViewById(R.id.temp_input_field)
+        humidify_input_field = findViewById(R.id.humidify_input_field)
+        frequency_input_field = findViewById(R.id.frequency_input_field)
+        light_intensity_input_field = findViewById(R.id.light_intensity_input_field)
+        colorSpinner = findViewById(R.id.colorSpinner)
+
+        // Инициализация полей вывода
+        temp_output_filler = findViewById(R.id.temp_output_filler)
+        humidify_output_filler = findViewById(R.id.humidify_output_filler)
+        frequency_output_filler = findViewById(R.id.frequency_output_filler)
+        light_intensity_output_filler = findViewById(R.id.light_intensity_output_filler)
+        color_input_card = findViewById(R.id.color_input_card)
+
+        temp_output_filler.text = getString(R.string.current_temp)
+        humidify_output_filler.text  = getString(R.string.reset_humidify)
+        frequency_output_filler.text  = getString(R.string.reset_frequency)
+        light_intensity_output_filler.text  = getString(R.string.reset_light_intencity)
+        color_input_card.setCardBackgroundColor(getResources().getColor(R.color.reset_input_color))
+
+        // Сброс параметров
+        temperature_input = null
+        humidify_input = null
+        frequency_input = null
+        light_intensity_input = null
+        color_input = null
+
+        // Сброс выходных параметров
+        temperature_output = null
+        humidify_output = null
+        frequency_output = null
+        light_intensity_output = null
+        color_output = null
+    }
+
+    private fun resetParams() {
+        // Сброс параметров
+        temperature_input = null
+        humidify_input = null
+        frequency_input = null
+        light_intensity_input = null
+        color_input = null
+
+        // Сброс выходных параметров
+        temperature_output = null
+        humidify_output = null
+        frequency_output = null
+        light_intensity_output = null
+        color_output = null
+
+        temp_output_filler.text = getString(R.string.current_temp)
+        humidify_output_filler.text  = getString(R.string.reset_humidify)
+        frequency_output_filler.text  = getString(R.string.reset_frequency)
+        light_intensity_output_filler.text  = getString(R.string.reset_light_intencity)
+        color_input_card.setCardBackgroundColor(getResources().getColor(R.color.reset_input_color))
+
+        temp_input_field.text = null
+        humidify_input_field.text = null
+        frequency_input_field.text = null
+        light_intensity_input_field.text = null
     }
 }
